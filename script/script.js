8ef15d8ab2dbc9165fc0423c49c3d0d576d35f4c
Salman Faris <salmanfs953@gmail.com>
2024-02-20 11:04:06 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// code for show collapse
+function showcollapse(){
+
+    document.getElementById("js").style.display="block";
+    document.getElementById("drop").style.display="none";
+    document.getElementById("close").style.display="block";
+}
+
+function hidecollapse(){
+    document.getElementById("js").style.display="none";
+    document.getElementById("drop").style.display="block";
+    document.getElementById("close").style.display="none";
+}
